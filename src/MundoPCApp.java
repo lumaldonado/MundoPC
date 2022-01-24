4f9ca25ac1f4563cdc48250e091133b4dcc41350
lumaldonado <lulimaldonado98@hotmail.com>
2022-01-24 12:18:07 -0300

    Se agregan las computadoras creadas a la orden y se llama al metodo mostrarOrden para mostrar en consola

--- src/MundoPCApp.java
+++ src/MundoPCApp.java
@@ -5,6 +5,17 @@ public class MundoPCApp {
         Raton ratonHP = new Raton("bluetooth", "HP");
         Computadora computadoraHP = new Computadora("Computadora HP", monitorHP, tecladoHP, ratonHP);
 
+        Monitor monitorGamer = new Monitor("Gamer", 34);
+        Teclado tecladoGamer = new Teclado("bluetooth", "Gamer");
+        Raton ratonGamer = new Raton("bluetooth", "Gamer");
+        Computadora computadoraGamer = new Computadora("Computadora Gamer", monitorGamer, tecladoGamer, ratonGamer);
+
+        Orden orden1 = new Orden();
+        orden1.agregarComputadora(computadoraGamer);
+        orden1.agregarComputadora(computadoraHP);
+
+        orden1.mostrarOrden();
+
     }
     
 }
