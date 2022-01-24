0fcfb9272e4a7cc21e1b19e943e8ce91bd48025f
lumaldonado <lulimaldonado98@hotmail.com>
2022-01-24 11:52:13 -0300

    Se crean las diferentes clases requeridas por el ejercicio. Tambien se agregan los atributos de computadora y dispositivo de entrada, a la vez que sus getters y setters

---
+++ src/Computadora.java
@@ -0,0 +1,68 @@
+public class Computadora {
+
+    private final int idComputadora;
+    private String nombre;
+    private Raton raton;
+    private Teclado teclado;
+    private Monitor monitor;
+    private static int contadorComputadoras;
+
+    private Computadora(){
+        this.idComputadora = ++Computadora.contadorComputadoras;
+    }
+
+    public Computadora(String nombre, Monitor monitor, Teclado teclado, Raton raton){
+        this();
+        this.nombre = nombre;
+        this.monitor = monitor;
+        this.teclado = teclado;
+        this.raton = raton;
+    }
+
+    public int getIdComputadora() {
+        return idComputadora;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Raton getRaton() {
+        return raton;
+    }
+
+    public void setRaton(Raton raton) {
+        this.raton = raton;
+    }
+
+    public Teclado getTeclado() {
+        return teclado;
+    }
+
+    public void setTeclado(Teclado teclado) {
+        this.teclado = teclado;
+    }
+
+    public Monitor getMonitor() {
+        return monitor;
+    }
+
+    public void setMonitor(Monitor monitor) {
+        this.monitor = monitor;
+    }
+
+    @Override
+    public String toString() {
+        return "Computadora{" +
+                "idComputadora=" + idComputadora +
+                ", nombre='" + nombre + '\'' +
+                ", raton=" + raton +
+                ", teclado=" + teclado +
+                ", monitor=" + monitor +
+                '}';
+    }
+}
