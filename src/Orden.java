d04f761a26924e071d4972fafee7fbe1b504e234
lumaldonado <lulimaldonado98@hotmail.com>
2022-01-24 11:57:11 -0300

    Se modifica el nombre de la clase teclado ya que estaba mal escrito y se agrega la clase madre de teclado y raton

--- src/Orden.java
+++ src/Orden.java
@@ -1,3 +1,8 @@
 public class Orden {
+
+    private final int idOrden;
+    private Computadora computadoras[];
+    private static int contadorOrdenes;
+    private static final int MAX_COMPUTADORAS = 10;
     
 }
